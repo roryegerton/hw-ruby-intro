3c5dc76f8e121e47f8e8f2f9a62c639c300947c7
roryegerton <roryegerton@gmail.com>
2017-06-07 00:08:15 +0100

    tidy tab

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,25 +3,25 @@
 # Part 1
 
 def sum arr
-	if arr.empty?
-		return 0
-	else
-	  return arr.inject{|sum_of_arr, elem| sum_of_arr += elem}
-	end
+  if arr.empty?
+    return 0
+  else
+    return arr.inject{|sum_of_arr, elem| sum_of_arr += elem}
+  end
 end
 
 def max_2_sum arr
   if arr.empty?
-		return 0
-	elsif arr.count == 1
-		return arr.first
-	else
-	  return arr.sort[-1] + arr.sort[-2]
-	end
+    return 0
+  elsif arr.count == 1
+    return arr.first
+  else
+    return arr.sort[-1] + arr.sort[-2]
+  end
 end
 
 def sum_to_n? arr, n
-	arr.each{ |n1| arr.each{ |n2| return true if (n1 + n2 == n) && (arr.index(n1) != arr.index(n2)) } }
+  arr.each{ |n1| arr.each{ |n2| return true if (n1 + n2 == n) && (arr.index(n1) != arr.index(n2)) } }
   false
 end
 
