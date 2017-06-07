95b7e388b59e888f16071e0b3de9be756b5611ec
roryegerton <roryegerton@gmail.com>
2017-06-07 23:13:58 +0100

    part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,21 +22,29 @@ end
 
 def sum_to_n? arr, n
   arr.each{ |n1| arr.each{ |n2| return true if (n1 + n2 == n) && (arr.index(n1) != arr.index(n2)) } }
-  false
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  unless s.empty?
+    letter = s[0].downcase if s[0] =~ /[A-Za-z]/
+    unless letter.nil?
+      return ["a","e","i","o","u"].include?(letter) ? false : true
+    end
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if /(\D+)/.match(s).nil? && !s.empty?
+    return s.to_i % 4 == 0 ? true : false
+  end
+  return false
 end
 
 # Part 3
