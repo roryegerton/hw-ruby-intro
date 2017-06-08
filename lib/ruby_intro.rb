c6bdf07a240a674160cdbe68449b7ed8ae7c65ec
roryegerton <roryegerton@gmail.com>
2017-06-08 08:16:33 +0100

    part 3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -50,5 +50,15 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    return sprintf "$%.2f", @price
+  end
 end
